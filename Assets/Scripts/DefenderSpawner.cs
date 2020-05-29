502fb5e35eeb9e671d761ff9369bef61371e1602
Sam Carty <samcarty311@gmail.com>
2020-05-29 17:24:06 +0100

    added click to spawn defender

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+    [SerializeField] GameObject wizard;
+
+    void OnMouseUp() {
+        
+        Instantiate(wizard, GetMouseWorldPosition(), Quaternion.identity);
+    }
+
+    Vector2 GetMouseWorldPosition() {
+        if (Camera.main == null) return new Vector2();
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+}
